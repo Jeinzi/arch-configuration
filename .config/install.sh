7a802bbee2948e3d1f86521ced245faf7c782933
Jeinzi <Jeinzi@gmx.de>
2017-02-12 15:33:52 +0100

    Added time zone change.

--- .config/install.sh
+++ .config/install.sh
@@ -5,7 +5,7 @@ cd Downloads
 mkdir -p install
 
 # Installation
-sudo pacman -S i3 xfce4-terminal git zsh compton lxappearance scrot sshfs firefox rofi thunderbird thunar veracrypt inkscape feh gimp texlive-most texmaker arc-gtk-theme
+sudo pacman -S i3 xfce4-terminal git zsh compton lxappearance scrot sshfs firefox rofi thunderbird thunar veracrypt inkscape feh gimp texlive-most texmaker arc-gtk-theme alsa-utils
 
 # Install yaourt
 curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
@@ -51,3 +51,6 @@ if ! xset q &>/dev/null; then
 	startx
 fi
 feh --bg-scale ~/Bilder/Wallpaper/forest.jpg
+
+# Set time zone
+timedatectl set-timezone Europe/Berlin
