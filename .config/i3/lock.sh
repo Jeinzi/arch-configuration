6d455abca4788ca37b367b2ea473c3bba27245de
Jeinzi <Jeinzi@gmx.de>
2017-02-13 03:52:11 +0100

    Added support for multiple screens.

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,11 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+IFS=$'\n'
 icon="$HOME/.config/i3/lock-icon.png"
 tmpBg='/tmp/screen.png'
 
-#(( $# )) && { icon=$1;  }
+#(( $# )) && { icon=$1; }
 
+# Get screenshot and pixelate.
 scrot "$tmpBg"
 convert "$tmpBg" -scale 10% -scale 1000% -colorspace gray "$tmpBg"
-convert "$tmpBg" "$icon" -gravity center -composite -matte "$tmpBg"
+
+# Get overlay icon size.
+iconSize+=($(file $icon | sed -e 's/^.*, \([0-9]*\) x \([0-9]*\).*/\1\n\2/'))
+
+# Get screen sizes.
+screenSizes+=($(bash ~/.config/i3/screenResolutions.sh))
+n=${#screenSizes[*]}
+
+# Overlay icon and screenshot for every screen.
+i=0
+xSum=0
+ySum=0
+while [ $i -lt $((n/2)) ]; do
+  let x=xSum+$((screenSizes[2*i]/2-iconSize[0]/2))
+  let y=$((screenSizes[2*i+1]/2-iconSize[1]/2))
+  convert "$tmpBg" "$icon" -geometry +$x+$y -composite "$tmpBg"
+  let xSum+=$((screenSizes[2*i]))
+  let i=i+1
+done
+
 i3lock -i "$tmpBg"
