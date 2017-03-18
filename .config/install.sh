003293413260282966d598885511ea867d738ef8
Jeinzi <Jeinzi@gmx.de>
2017-03-18 23:56:40 +0100

    Added mplayer.

--- .config/install.sh
+++ .config/install.sh
@@ -8,7 +8,7 @@ cd Downloads
 mkdir -p install
 
 # Installation
-sudo pacman -S i3 xfce4-terminal git zsh compton lxappearance scrot sshfs firefox rofi thunderbird thunar veracrypt inkscape feh gimp texlive-most texmaker arc-gtk-theme alsa-utils nm-applet gst-libav
+sudo pacman -S i3 xfce4-terminal git zsh compton lxappearance scrot sshfs firefox rofi thunderbird thunar veracrypt inkscape feh gimp texlive-most texmaker arc-gtk-theme alsa-utils nm-applet gst-libav mplayer
 
 # Install yaourt
 curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
