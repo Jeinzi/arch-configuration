4db843d74367e2476b388a784104cc36814d87cb
Jeinzi <Jeinzi@gmx.de>
2017-10-14 20:44:22 +0200

    Added shortcut to switch wallpaper and modified corresponding script.

--- .config/i3/wallpaperScript.sh
+++ .config/i3/wallpaperScript.sh
@@ -1,4 +1,28 @@
+# File that triggers next wallpaper on creation
+file=~/.config/i3/next
+# Delay between wallpapers in minutes
+delay=15
+delay=$[delay*60]
+# Switch wallpaper once on startup.
+touch $file
+
 while true; do
-	feh --bg-fill --randomize --no-fehbg ~/Bilder/Verschiedenes/Wallpaper/Minimalism
-	sleep 15m
+    echo "Enter loop."
+    # Remove file if it exists.
+    fileExists=0
+    if [ -f "$file" ]; then
+        echo "File exists."
+        fileExists=1
+        rm $file
+    fi
+
+    # Switch wallpaper.
+    if [ $fileExists -eq 1 ] || [ $SECONDS -ge delay ]; then
+       feh --bg-fill --randomize --no-fehbg ~/Bilder/Verschiedenes/Wallpaper/Minimalism
+    fi
+
+	SECONDS=0
+    echo "inotifywait"
+    inotifywait -qqt $delay -e create "$(dirname $file)"
+    echo "inotifywait quit"
 done
