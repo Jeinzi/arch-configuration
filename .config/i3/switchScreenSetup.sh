4142b7af1f12012def817e09a1448376a6854ae4
Jeinzi <Jeinzi@gmx.de>
2019-01-20 19:01:18 +0100

    Added screen setup to rofi.

--- .config/i3/switchScreenSetup.sh
+++ .config/i3/switchScreenSetup.sh
@@ -1,105 +1,37 @@
 #!/bin/bash
-# Gratefully taken from https://github.com/carnager/rofi-scripts/blob/master/monitor_layout.sh
-# and adapted for integration in rofi without -dmenu option.
 
-XRANDR=$(which xrandr)
-
-MONITORS=( $( ${XRANDR} | awk '( $2 == "connected" ){ print $1 }' ) )
-
-
-NUM_MONITORS=${#MONITORS[@]}
-
-TITLES=()
-COMMANDS=()
-
-
-function gen_xrandr_only()
-{
-    selected=$1
-
-    cmd="xrandr --output ${MONITORS[$selected]} --auto "
-
-    for entry in $(seq 0 $((${NUM_MONITORS}-1)))
-    do
-        if [ $selected != $entry ]
-        then
-            cmd="$cmd --output ${MONITORS[$entry]} --off"
-        fi
-    done
-
-    echo $cmd
-}
-
-
-
-declare -i index=0
-TILES[$index]="Cancel"
-COMMANDS[$index]="true"
-index+=1
-
-
-for entry in $(seq 0 $((${NUM_MONITORS}-1)))
-do
-    TILES[$index]="Only ${MONITORS[$entry]}"
-    COMMANDS[$index]=$(gen_xrandr_only $entry)
-    index+=1
-done
-
-##
-# Dual screen options
-##
-for entry_a in $(seq 0 $((${NUM_MONITORS}-1)))
-do
-    for entry_b in $(seq 0 $((${NUM_MONITORS}-1)))
-    do
-        if [ $entry_a != $entry_b ]
-        then
-            TILES[$index]="Dual Screen ${MONITORS[$entry_a]} -> ${MONITORS[$entry_b]}"
-            COMMANDS[$index]="xrandr --output ${MONITORS[$entry_a]} --auto \
-                              --output ${MONITORS[$entry_b]} --auto --left-of ${MONITORS[$entry_a]}"
-
-            index+=1
-        fi
-    done
-done
-
-
-##
-# Clone monitors
-##
-for entry_a in $(seq 0 $((${NUM_MONITORS}-1)))
-do
-    for entry_b in $(seq 0 $((${NUM_MONITORS}-1)))
-    do
-        if [ $entry_a != $entry_b ]
-        then
-            TILES[$index]="Clone Screen ${MONITORS[$entry_a]} -> ${MONITORS[$entry_b]}"
-            COMMANDS[$index]="xrandr --output ${MONITORS[$entry_a]} --auto \
-                              --output ${MONITORS[$entry_b]} --auto --same-as ${MONITORS[$entry_a]}"
-
-            index+=1
-        fi
-    done
-done
-
+entries=(Single-Screen Dual-Screen)
+commands=(
+~/.screenlayout/single.sh
+~/.screenlayout/dual.sh
+)
 
 ##
 #  Generate entries, where first is key.
 ##
-function gen_entries()
+function show_entries()
 {
-    for a in $(seq 0 $(( ${#TILES[@]} -1 )))
+    for e in "${entries[@]}"
     do
-        echo $a ${TILES[a]}
+        echo $e
     done
 }
 
 # Check for command line arguments.
 if [[ -z "$@" ]]; then
     # Echo menu entries if nothing was passed.
-    gen_entries
+    show_entries
 else
     # Execute xrandr command corresponding to the chosen menu entry.
-    SEL=$(echo $@ | awk '{print $1}')
-    $( ${COMMANDS[$SEL]} )
+    i=0
+    for e in "${entries[@]}"
+    do
+        if [[ "$@" == "${entries[$i]}" ]]; then
+            break
+        fi;
+        ((i++))
+    done
+    $( ${commands[$i]} )
+    i3-msg restart
+    touch ~/.config/i3/next
 fi
